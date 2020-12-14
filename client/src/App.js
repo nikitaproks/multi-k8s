8a9a17dab688c5f89813ff5220af4bbd347de234
nikita <prokayev@icloud.com>
2020-12-14 22:20:14 +0200

    added corrado

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Corrado gay
+            Corrado gay 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
