6cf7a7c34499cd7fe4064212971567113e0a8e67
nikita <prokayev@icloud.com>
2020-12-14 23:22:01 +0200

    changes some were made

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Corrado lol nikita
-          </a>
+          <h1 className="App-title">Fib Calculator version KUBERNETES!</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
