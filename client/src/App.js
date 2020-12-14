65d1535458b0c18bb812d9a0517dafeebbf7b5d3
nikita <prokayev@icloud.com>
2020-12-14 22:38:41 +0200

    dadfdsgfdl lol

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Lol no
+            Corrado lol 345
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
