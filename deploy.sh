c58e0bfbb72474cb905038ab555b0a45e7ea5b47
nikita <prokayev@icloud.com>
2020-12-15 10:24:41 +0200

    changed docker images tagging

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-docker built -t nikitaproks/multi-client:latest -f ./client/Dockerfile ./client
-docker built -t nikitaproks/multi-server:latest -f ./server/Dockerfile ./server
-docker built -t nikitaproks/multi-worker:latest -f ./worker/Dockerfile ./worker
+docker built -t nikitaproks/multi-client:$SHA  -f ./client/Dockerfile ./client
+docker built -t nikitaproks/multi-server:$SHA  -f ./server/Dockerfile ./server
+docker built -t nikitaproks/multi-worker:$SHA  -f ./worker/Dockerfile ./worker
 
-docker tag nikitaproks/multi-client:latest nikitaproks/multi-client:$SHA 
-docker tag nikitaproks/multi-server:latest nikitaproks/multi-server:$SHA 
-docker tag nikitaproks/multi-worker:latest nikitaproks/multi-worker:$SHA
+#docker tag nikitaproks/multi-client:latest nikitaproks/multi-client:$SHA 
+#docker tag nikitaproks/multi-server:latest nikitaproks/multi-server:$SHA 
+#docker tag nikitaproks/multi-worker:latest nikitaproks/multi-worker:$SHA
 
-docker push nikitaproks/multi-client:latest
-docker push nikitaproks/multi-server:latest
-docker push nikitaproks/multi-worker:latest
+#docker push nikitaproks/multi-client:latest
+#docker push nikitaproks/multi-server:latest
+#docker push nikitaproks/multi-worker:latest
 
 docker push nikitaproks/multi-client:$SHA
 docker push nikitaproks/multi-server:$SHA
