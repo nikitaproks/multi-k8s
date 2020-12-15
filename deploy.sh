c77eb1551ed29921ce192f93c69c5966b0bb8484
nikita <prokayev@icloud.com>
2020-12-15 10:32:36 +0200

    changed docker images tagging

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,16 @@
-docker built -t nikitaproks/multi-client:latest  -f ./client/Dockerfile ./client
-docker built -t nikitaproks/multi-server:latest   -f ./server/Dockerfile ./server
-docker built -t nikitaproks/multi-worker:latest   -f ./worker/Dockerfile ./worker
+docker built --tag nikitaproks/multi-client:latest --tag nikitaproks/multi-client:$SHA -f ./client/Dockerfile ./client
+docker built --tag nikitaproks/multi-server:latest --tag nikitaproks/multi-server:$SHA -f ./server/Dockerfile ./server
+docker built --tag nikitaproks/multi-worker:latest --tag nikitaproks/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-#docker tag nikitaproks/multi-client:latest nikitaproks/multi-client:$SHA 
-#docker tag nikitaproks/multi-server:latest nikitaproks/multi-server:$SHA 
-#docker tag nikitaproks/multi-worker:latest nikitaproks/multi-worker:$SHA
+docker push nikitaproks/multi-client:latest
+docker push nikitaproks/multi-server:latest
+docker push nikitaproks/multi-worker:latest
 
-#docker push nikitaproks/multi-client:latest
-#docker push nikitaproks/multi-server:latest
-#docker push nikitaproks/multi-worker:latest
-
-docker push nikitaproks/multi-client:latest 
-docker push nikitaproks/multi-server:latest 
-docker push nikitaproks/multi-worker:latest 
+docker push nikitaproks/multi-client:$SHA
+docker push nikitaproks/multi-server:$SHA
+docker push nikitaproks/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=nikitaproks/multi-server:latest 
-kubectl set image deployments/client-deployment client=nikitaproks/multi-client:latest 
-kubectl set image deployments/worker-deployment worker=nikitaproks/multi-worker:latest 
+kubectl set image deployments/server-deployment server=nikitaproks/multi-server:$SHA
+kubectl set image deployments/client-deployment client=nikitaproks/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=nikitaproks/multi-worker:$SHA
